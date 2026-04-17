551b16d37c329c4400b8252824d7f92b08bd35b4
anaskhan010 <anas@sentrixmedia.com>
2026-04-17 16:33:22 +0500

    new

--- config/Connection.js
+++ config/Connection.js
@@ -1,11 +1,15 @@
 const mysql = require("mysql2/promise");
 
 const dbConfig = {
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "visa",
-  port: Number(process.env.DB_PORT || 3306),
+//   host: "localhost",
+//   user: "root",
+//   password:  "",
+//   database:  "visa",
+host: "37.27.187.4",
+  user: "root",
+  password: "l51Qh6kM2vb3npALukrKNMzNAlBogTj0NSH4Gd3IxqMfaP0qfFkp54e7jcknqGNX",
+  database: "ai_agent",
+  port:  3306,
 };
 
 const pool = mysql.createPool(dbConfig);
