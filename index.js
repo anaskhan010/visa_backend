04359c2944f7e09ae906996c11aed4761f01769a
anaskhan010 <anas@sentrixmedia.com>
2026-04-17 16:44:54 +0500

    new

--- index.js
+++ index.js
@@ -39,6 +39,9 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 
